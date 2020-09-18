7f174f2ebb73d136f8d383aa23db74d53ca3b8d7
Osmund <21172332+osmundf@users.noreply.github.com>
2020-09-18 12:37:29 +0200

    Added chess piece caste.

---
+++ main-java/com/github/osmundf/chess/hub/Caste.java
@@ -0,0 +1,83 @@
+package com.github.osmundf.chess.hub;
+
+/**
+ * Chess piece caste.
+ */
+public enum Caste {
+
+    BISHOP,
+    KING,
+    KNIGHT,
+    NONE,
+    PAWN,
+    QUEEN,
+    ROOK;
+
+    /** Returns piece material value. */
+    public int value() {
+        switch (this) {
+            case KING:
+                return 300;
+            case QUEEN:
+                return 9;
+            case ROOK:
+                return 5;
+            case KNIGHT:
+            case BISHOP:
+                return 3;
+            case PAWN:
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
+    /** Returns piece index. */
+    public int index() {
+        switch (this) {
+            case PAWN:
+                return 1;
+            case BISHOP:
+                return 2;
+            case KNIGHT:
+                return 3;
+            case ROOK:
+                return 4;
+            case QUEEN:
+                return 5;
+            case KING:
+                return 6;
+            default:
+                return 0;
+        }
+    }
+
+    /**
+     * Chess piece caste factory method.
+     *
+     * @param index caste index
+     * @return Returns chess piece caste based on index value.
+     */
+    public static Caste fromIndex(int index) {
+        switch (index) {
+            case 6:
+                return KING;
+            case 5:
+                return QUEEN;
+            case 4:
+                return ROOK;
+            case 3:
+                return KNIGHT;
+            case 2:
+                return BISHOP;
+            case 1:
+                return PAWN;
+            case 0:
+                return NONE;
+            default: {
+                var cause = new ChessException("index: " + index);
+                throw new ChessException("chess.kind.invalid.index", cause);
+            }
+        }
+    }
+}
---
+++ test-java/com/github/osmundf/chess/hub/CasteTest.java
@@ -0,0 +1,35 @@
+package com.github.osmundf.chess.hub;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CasteTest {
+
+    @Test
+    void testEnumSize() {
+        assertEquals(7, Caste.values().length);
+    }
+
+    @Test
+    void testValue() {
+        assertEquals(300, Caste.KING.value());
+        assertEquals(9, Caste.QUEEN.value());
+        assertEquals(5, Caste.ROOK.value());
+        assertEquals(3, Caste.BISHOP.value());
+        assertEquals(3, Caste.KNIGHT.value());
+        assertEquals(1, Caste.PAWN.value());
+        assertEquals(0, Caste.NONE.value());
+    }
+
+    @Test
+    void testIndex() {
+        assertEquals(6, Caste.KING.index());
+        assertEquals(5, Caste.QUEEN.index());
+        assertEquals(4, Caste.ROOK.index());
+        assertEquals(3, Caste.KNIGHT.index());
+        assertEquals(2, Caste.BISHOP.index());
+        assertEquals(1, Caste.PAWN.index());
+        assertEquals(0, Caste.NONE.index());
+    }
+}
