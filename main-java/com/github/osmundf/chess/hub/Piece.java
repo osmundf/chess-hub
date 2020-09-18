9048aa579314506bcf7d9448f9fcd126b2f6fc2a
Osmund <21172332+osmundf@users.noreply.github.com>
2020-09-18 13:14:29 +0200

    Added chess piece.

---
+++ main-java/com/github/osmundf/chess/hub/Piece.java
@@ -0,0 +1,92 @@
+package com.github.osmundf.chess.hub;
+
+/**
+ * Chess piece.
+ */
+public class Piece {
+
+    public static Piece newPiece(Side side, Caste caste, Square square) {
+        if (side == null) {
+            var cause = new ChessException("chess.side.null");
+            throw new ChessException("chess.piece.new.piece.null.argument", cause);
+        }
+        if (Side.NO_SIDE == side) {
+            var cause = new ChessException("chess.side.none");
+            throw new ChessException("chess.piece.new.piece.no.side", cause);
+        }
+        if (caste == null) {
+            var cause = new ChessException("chess.caste.null");
+            throw new ChessException("chess.piece.new.piece.null.argument", cause);
+        }
+        if (Caste.NONE == caste) {
+            var cause = new ChessException("chess.caste.none");
+            throw new ChessException("chess.piece.new.piece.no.caste", cause);
+        }
+        if (square == null) {
+            var cause = new ChessException("chess.square.null");
+            throw new ChessException("chess.piece.new.piece.null.argument", cause);
+        }
+        return new Piece(side, caste, square);
+    }
+
+    private final Side side;
+
+    private final Caste caste;
+
+    private final Square square;
+
+    /**
+     * Chess piece constructor (package-private).
+     *
+     * @param side   piece side
+     * @param caste  piece caste
+     * @param square piece square
+     */
+    Piece(Side side, Caste caste, Square square) {
+        this.side = side;
+        this.caste = caste;
+        this.square = square;
+    }
+
+    /** Returns the chess piece side. */
+    public Side side() {
+        return side;
+    }
+
+    /** Returns the chess piece caste. */
+    public Caste caste() {
+        return caste;
+    }
+
+    /** Returns the chess piece square. */
+    public Square square() {
+        return square;
+    }
+
+    /** Returns chess piece index. */
+    public int index() {
+        // side[ss] caste[ccc] rank[rrr] file[fff]
+        int result = side.index() << 9;
+        result |= (caste.index() << 6);
+        result |= square.index();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Piece)) {
+            return false;
+        }
+        return this == object || this.index() == ((Piece) object).index();
+    }
+
+    @Override
+    public int hashCode() {
+        return index();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s_%s(%s)", side, caste, square);
+    }
+}
---
+++ test-java/com/github/osmundf/chess/hub/PieceTest.java
@@ -0,0 +1,176 @@
+package com.github.osmundf.chess.hub;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class PieceTest {
+
+    @Test
+    void testRange() {
+        var squareArray = new Square[64];
+        for (var file = 'a'; file <= 'h'; file++) {
+            for (var rank = 1; rank <= 8; rank++) {
+                var square = Square.newSquare(file, rank);
+                squareArray[square.index()] = square;
+            }
+        }
+
+        for (var side : Side.values()) {
+            if (Side.NO_SIDE == side) {
+                continue;
+            }
+            for (var caste : Caste.values()) {
+                if (Caste.NONE == caste) {
+                    continue;
+                }
+                for (var square : squareArray) {
+                    var piece = Piece.newPiece(side, caste, square);
+
+                    assertEquals(piece.side(), side);
+                    assertEquals(piece.caste(), caste);
+                    assertEquals(piece.square(), square);
+                }
+            }
+        }
+    }
+
+    @Test
+    void testIndex() {
+        var squareArray = new Square[64];
+        for (var file = 'a'; file <= 'h'; file++) {
+            for (var rank = 1; rank <= 8; rank++) {
+                var square = Square.newSquare(file, rank);
+                squareArray[square.index()] = square;
+            }
+        }
+
+        for (var side : Side.values()) {
+            if (Side.NO_SIDE == side) {
+                continue;
+            }
+            for (var caste : Caste.values()) {
+                if (Caste.NONE == caste) {
+                    continue;
+                }
+                for (var square : squareArray) {
+                    var piece = Piece.newPiece(side, caste, square);
+                    var expected = (side.index() << 9) + (caste.index() << 6) + square.index();
+                    assertEquals(expected, piece.index());
+                    assertEquals(expected, piece.hashCode());
+                }
+            }
+        }
+    }
+
+    @Test
+    void testNoSideException() {
+        var squareArray = new Square[64];
+        for (var file = 'a'; file <= 'h'; file++) {
+            for (var rank = 1; rank <= 8; rank++) {
+                var square = Square.newSquare(file, rank);
+                squareArray[square.index()] = square;
+            }
+        }
+
+        for (var caste : Caste.values()) {
+            if (Caste.NONE == caste) {
+                continue;
+            }
+            for (var square : squareArray) {
+                try {
+                    var piece = Piece.newPiece(null, caste, square);
+                    fail("piece.test.expected.runtime.exception.null.side");
+                }
+                catch (RuntimeException e) {
+                    assertEquals(ChessException.class.getName(), e.getClass().getName());
+                    assertEquals("chess.piece.new.piece.null.argument", e.getMessage());
+                    var cause = e.getCause();
+                    assertNotNull(cause);
+                    assertEquals("chess.side.null", cause.getMessage());
+                }
+
+                try {
+                    var piece = Piece.newPiece(Side.NO_SIDE, caste, square);
+                    fail("piece.test.expected.runtime.exception.no.side");
+                }
+                catch (RuntimeException e) {
+                    assertEquals(ChessException.class.getName(), e.getClass().getName());
+                    assertEquals("chess.piece.new.piece.no.side", e.getMessage());
+                    var cause = e.getCause();
+                    assertNotNull(cause);
+                    assertEquals("chess.side.none", cause.getMessage());
+                }
+            }
+        }
+    }
+
+    @Test
+    void testNoCasteException() {
+        var squareArray = new Square[64];
+        for (var file = 'a'; file <= 'h'; file++) {
+            for (var rank = 1; rank <= 8; rank++) {
+                var square = Square.newSquare(file, rank);
+                squareArray[square.index()] = square;
+            }
+        }
+
+        for (var side : Side.values()) {
+            if (Side.NO_SIDE == side) {
+                continue;
+            }
+            for (var square : squareArray) {
+                try {
+                    var piece = Piece.newPiece(side, null, square);
+                    fail("piece.test.expected.runtime.exception.null.caste");
+                }
+                catch (RuntimeException e) {
+                    assertEquals(ChessException.class.getName(), e.getClass().getName());
+                    assertEquals("chess.piece.new.piece.null.argument", e.getMessage());
+                    var cause = e.getCause();
+                    assertNotNull(cause);
+                    assertEquals("chess.caste.null", cause.getMessage());
+                }
+
+                try {
+                    var piece = Piece.newPiece(side, Caste.NONE, square);
+                    fail("piece.test.expected.runtime.exception.no.caste");
+                }
+                catch (RuntimeException e) {
+                    assertEquals(ChessException.class.getName(), e.getClass().getName());
+                    assertEquals("chess.piece.new.piece.no.caste", e.getMessage());
+                    var cause = e.getCause();
+                    assertNotNull(cause);
+                    assertEquals("chess.caste.none", cause.getMessage());
+                }
+            }
+        }
+    }
+
+    @Test
+    void testNoSquareException() {
+        for (var side : Side.values()) {
+            if (Side.NO_SIDE == side) {
+                continue;
+            }
+            for (var caste : Caste.values()) {
+                if (Caste.NONE == caste) {
+                    continue;
+                }
+                try {
+                    var piece = Piece.newPiece(side, caste, null);
+                    fail("piece.test.expected.runtime.exception.null.square");
+                }
+                catch (RuntimeException e) {
+                    assertEquals(ChessException.class.getName(), e.getClass().getName());
+                    assertEquals("chess.piece.new.piece.null.argument", e.getMessage());
+                    var cause = e.getCause();
+                    assertNotNull(cause);
+                    assertEquals("chess.square.null", cause.getMessage());
+                }
+            }
+        }
+    }
+}
