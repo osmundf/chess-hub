e62c8deaf0404a62ec484802ba0dde2e4791c4e7
Osmund <21172332+osmundf@users.noreply.github.com>
2020-09-21 21:36:55 +0200

    Complete side enum code coverage.

--- test-java/com/github/osmundf/chess/hub/SideTest.java
+++ test-java/com/github/osmundf/chess/hub/SideTest.java
@@ -3,6 +3,9 @@ package com.github.osmundf.chess.hub;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SideTest {
 
@@ -16,5 +19,26 @@ class SideTest {
         assertEquals(2, Side.WHITE.index());
         assertEquals(1, Side.BLACK.index());
         assertEquals(0, Side.NO_SIDE.index());
+    }
+
+    @Test
+    void testFlags() {
+        for (var side : Side.values()) {
+            if (Side.NO_SIDE == side) {
+                assertFalse(side.isWhite());
+                assertFalse(side.isBlack());
+                assertSame(side, side.opposite());
+            }
+            else if (Side.WHITE == side) {
+                assertTrue(side.isWhite());
+                assertFalse(side.isBlack());
+                assertSame(Side.BLACK, side.opposite());
+            }
+            else if (Side.BLACK == side) {
+                assertFalse(side.isWhite());
+                assertTrue(side.isBlack());
+                assertSame(Side.WHITE, side.opposite());
+            }
+        }
     }
 }
